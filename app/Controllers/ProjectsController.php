c37901f2b0c9cf99287cde5285ed410c005afe35
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-14 04:32:43 +0530

    view returned

--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -20,9 +20,7 @@ class ProjectsController extends BaseController {
     public function index() {
         $data['projects'] = $this->ProjectsModel->read_data();
         $data['dash']   = $this->ProjectsModel->dashboard();
-        echo "<pre>";
-        print_r($data['dash']);
-        // return view('projects/list',$data);
+        return view('projects/list',$data);
     }
     public function create() {
         $data['mode'] = 'create';
