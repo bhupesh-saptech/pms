6683aed1b43ad3e97608c496f13784df73eb02ea
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-14 12:42:54 +0530

    dashboard comments

--- app/Views/agents/list.php
+++ app/Views/agents/list.php
@@ -1,7 +1,7 @@
 <?=  $this->extend('layouts/base'); ?>
-<?=  $this->section("dashboard"); ?>
+<!-- <?=  $this->section("dashboard"); ?>
 <?= $this->include('layouts/dashboard'); ?>
-<?=  $this->endSection(); ?>
+<?=  $this->endSection(); ?> -->
 <?=  $this->section("content"); ?>
 
 <div class="container">
