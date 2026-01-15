a027e0b64bf03f98c0fb234d86ede95386799913
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-15 07:18:53 +0530

    routes changed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,50 +8,48 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->get("/","Home::index");
 
-$routes->get("/agents","AgentsController::index");
-$routes->get("/agents/create","AgentsController::create");
-$routes->post("/agents/create","AgentsController::create");
-$routes->get("/agents/edit/(:num)","AgentssController::edit/$1");
-$routes->get("/agents/view/(:num)","AgentssController::view/$1");
-$routes->get("/agents/delete/(:num)","AgentssController::delete/$1");
 
-$routes->get("/clients","ClientsController::index");
-$routes->get("/clients/create","ClientsController::create");
-$routes->post("/clients/create","ClientsController::create");
-$routes->get("/clients/edit/(:num)","ClientsController::edit/$1");
-$routes->get("/clients/view/(:num)","ClientsController::view/$1");
-$routes->get("/clients/delete/(:num)","ClientsController::delete/$1");
-
-$routes->get("/issues","IssuesController::index");
-$routes->get("/issues/create","IssuesController::create");
-$routes->post("/issues/create","IssuesController::create");
-$routes->get("/issues/edit/(:num)","IssuesController::edit/$1");
-$routes->get("/issues/view/(:num)","IssuesController::view/$1");
-$routes->get("/issues/delete/(:num)","IssuesController::delete/$1");
+$routes->get(                 "/agents",              "AgentsController::index");
+$routes->get(                 "/agents/read/(:num)",  "AgentsController::read/$1");
+$routes->match(['get','post'],"/agents/create",       "AgentsController::create");
+$routes->match(['get','post'],"/agents/update/(:num)","AgentsController::update/$1");
+$routes->match(['get','post'],"/agents/delete/(:num)","AgentsController::delete/$1");
 
 
-$routes->get("/projects","ProjectsController::index");
-$routes->get("/projects/read/(:num)",                   "ProjectsController::read/$1");
+$routes->get(                 "/clients",               "ClientsController::index");
+$routes->get(                 "/clients/read/(:num)",   "ClientsController::read/$1");
+$routes->match(['get','post'],"/clients/create",       "ClientsController::create");
+$routes->match(['get','post'],"/clients/update/(:num)","ClientsController::update/$1");
+$routes->match(['get','post'],"/clients/delete/(:num)","ClientsController::delete/$1");
+
+
+$routes->get(                 "/issues",              "IssuesController::index");
+$routes->get(                 "/issues/read/(:num)",  "IssuesController::read/$1");
+$routes->match(['get','post'],"/issues/create",       "IssuesController::create");
+$routes->match(['get','post'],"/issues/update/(:num)","IssuesController::update/$1");
+$routes->match(['get','post'],"/issues/delete/(:num)","IssuesController::delete/$1");
+
+
+$routes->get(                 "/projects",              "ProjectsController::index");
+$routes->get(                 "/projects/read/(:num)",  "ProjectsController::read/$1");
 $routes->match(['get','post'],"/projects/create",       "ProjectsController::create");
 $routes->match(['get','post'],"/projects/update/(:num)","ProjectsController::update/$1");
 $routes->match(['get','post'],"/projects/delete/(:num)","ProjectsController::delete/$1");
 
-$routes->get("/tasks","TasksController::index");
-$routes->get("/tasks/create","TasksController::create");
-$routes->post("/tasks/create","TasksController::create");
-$routes->get("/tasks/edit/(:num)","TasksController::edit/$1");
-$routes->get("/tasks/view/(:num)","TasksController::view/$1");
-$routes->get("/tasks/delete/(:num)","TasksController::delete/$1");
+$routes->get("/tasks",                               "TasksController::index");
+$routes->get("/tasks/read/(:num)",                   "TasksController::read/$1");
+$routes->match(['get','post'],"/tasks/create",       "TasksController::create");
+$routes->match(['get','post'],"/tasks/update/(:num)","TasksController::update/$1");
+$routes->match(['get','post'],"/tasks/delete/(:num)","TasksController::delete/$1");
 
+$routes->get(                 "/users",              "UsersController::index");
+$routes->get(                 "/users/read/(:num)",  "UsersController::read/$1");
+$routes->match(['get','post'],"/users/create",       "UsersController::create");
+$routes->match(['get','post'],"/users/update/(:num)","UsersController::update/$1");
+$routes->match(['get','post'],"/users/delete/(:num)","UsersController::delete/$1");
 
-$routes->get("/users","UsersController::index");
 $routes->get("/users/login","UsersController::login");
 $routes->post("/users/login","UsersController::login");
 $routes->get("/users/logout","UsersController::logout");
-$routes->get("/users/create","UsersController::create");
-$routes->post("/users/create","UsersController::create");
-$routes->get("/users/edit/(:num)","UsersController::edit/$1");
-$routes->get("/users/view/(:num)","UsersController::view/$1");
-$routes->get("/users/delete/(:num)","UsersController::delete/$1");
 
 return $routes;
