774a4fcd436d4af2ed9f54dfac15f7873949824d
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-15 09:37:46 +0530

    methods added in tasks

--- app/Controllers/TasksController.php
+++ app/Controllers/TasksController.php
@@ -10,28 +10,26 @@ use App\Models\IssuesModel;
 use App\Models\TasksModel;
 
 class TasksController extends BaseController {
-    public $AgentsModel;
-    public $ProjectsModel;
-    public $IssuesModel; 
-    public $TasksModel;
+    protected $AgentsModel,$ProjectsModel,$IssuesModel,$TasksModel;
+    protected $data;
     public function __construct() {
         helper('form');
         $this->AgentsModel   = new AgentsModel();
         $this->ProjectsModel = new ProjectsModel();
         $this->IssuesModel   = new IssuesModel();
         $this->TasksModel    = new TasksModel;
+        $this->data['tasks'] = $this->TasksModel->read_data();
+        $this->data['dash']  = $this->TasksModel->dashboard();
+        $this->data['agents']   = $this->AgentsModel->select("agent_id,agent_nm")->orderBy("agent_id")->findAll();
+        $this->data['projects'] = $this->ProjectsModel->select("proj_id,ps_name")->orderBy("proj_id")->findAll();
+        $this->data['issues']   = $this->IssuesModel->select("issue_id,is_title")->orderBy("issue_id")->findAll();
     }
     public function index() {
-        $data['tasks'] = $this->TasksModel->read_data();
-        $data['dash']   = $this->TasksModel->dashboard();
-        return view('tasks/tasksList',$data);
+        return view('tasks/tasksList',$this->data);
     }
     public function create() {
         $data['mode'] = 'create';
-        $data['agents']   = $this->AgentsModel->select("agent_id,agent_nm")->orderBy("agent_id")->findAll();
-        $data['projects'] = $this->ProjectsModel->select("proj_id,ps_name")->orderBy("proj_id")->findAll();
-        $data['issues']   = $this->IssuesModel->select("issue_id,is_title")->orderBy("issue_id")->findAll();
-         if ($this->request->is('post')) {
+        if ($this->request->is('post')) {
             $form = [
                 'ts_desc' => $this->request->getPost('ts_desc'),
                 'proj_id' => $this->request->getPost('proj_id'),
@@ -42,10 +40,39 @@ class TasksController extends BaseController {
                 return redirect()->to('/tasks')->with('message',"Data Inserted Succefully");
             } else {
                 $data['errors'] = $this->TasksModel->errors();     
-                return view('tasks/tasksForm', $data);  
+                return view('tasks/tasksForm', $this->data);  
             }
         } else {
-            return view('tasks/tasksForm',$data);
+            return view('tasks/tasksForm',$this->data);
         }
+    }
+    public function read($task_id) {
+        $this->data['mode'] = 'read';
+        $this->data['task'] = $this->TasksModel->find($task_id);
+        return view('tasks/tasksForm',$this->data);
+    }
+    public function update() {
+        $data['mode'] = 'update';
+        if ($this->request->is('post')) {
+            $form = [
+                'ts_desc' => $this->request->getPost('ts_desc'),
+                'proj_id' => $this->request->getPost('proj_id'),
+                'agent_id' => $this->request->getPost('agent_id'),
+                
+            ];
+            if($this->TasksModel->update($form,false)) {
+                return redirect()->to('/tasks')->with('message',"Data updated Succefully");
+            } else {
+                $data['errors'] = $this->TasksModel->errors();     
+                return view('tasks/tasksForm', $this->data);  
+            }
+        } else {
+            return view('tasks/tasksForm',$this->data);
+        }
+    }
+    public function delete($task_id) {
+        $this->data['mode'] = 'delete';
+        $this->data['task'] = $this->TasksModel->find($task_id);
+        return view('tasks/tasksForm',$this->data);
     }
 }
