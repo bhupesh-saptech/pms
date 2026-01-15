e023d8293355e248804fcfdd8f12545878c8eccc
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-15 13:14:14 +0530

    icons reagged

--- app/Views/tasks/tasksForm.php
+++ app/Views/tasks/tasksForm.php
@@ -7,8 +7,8 @@
         <div class="mb-3">
             <h4 class="mb-4 text-center">Task Creation</h4>                 
             <button type="button" class="btn btn-primary float-end "><i class="fa fa-arrow-left"></i></button> 
-            <button type="button" class="btn btn-primary float-end me-3"><i class="fa fa-edit"></i></button>
             <button type="button" class="btn btn-primary float-end me-3"><i class="fa fa-trash"></i></button>
+            <button type="button" class="btn btn-primary float-end me-3"><i class="fa fa-edit"></i></button>
         </div>
         <div class="form-group">
             <label class="form-label">Task ID</label>
