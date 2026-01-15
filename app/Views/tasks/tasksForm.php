a391c838f272ff0922d4a4fcae2c8cab516cc97f
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-15 10:10:07 +0530

    alignment at end

--- app/Views/tasks/tasksForm.php
+++ app/Views/tasks/tasksForm.php
@@ -5,7 +5,7 @@
 <div class="col-sm-8">
     <form method="post"  id="form">
         <div class="mb-3">         
-            <button type="submit" class="btn btn-primary"><i class="fa fa-edit"></i></button>
+            <button type="submit" class="btn btn-primary float-end"><i class="fa fa-edit"></i></button>
         </div>
         <div class="form-group">
             <label class="form-label">Task ID</label>
