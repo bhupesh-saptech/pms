9be394463a40de7caa327bc32e4e5b57d11ddb13
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-15 13:36:40 +0530

    Title Altered

--- app/Views/issues/issuesForm.php
+++ app/Views/issues/issuesForm.php
@@ -6,7 +6,7 @@
     <form method="post" id="form">
         <div class="row">
             <div class="col-sm-9">
-                <h4 class="text-center">Task Creation</h4>
+                <h4 class="text-center">Issue Creation</h4>
             </div>
             <div class="col-sm-3">                 
                 <button type="button" class="btn btn-primary float-end " onclick="history.back();" ><i class="fa fa-arrow-left"></i></button> 
