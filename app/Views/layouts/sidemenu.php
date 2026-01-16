afa550339c954844e9bf65df8229810cbf44d7cf
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-17 03:03:51 +0530

    side menu updated

--- app/Views/layouts/sidemenu.php
+++ app/Views/layouts/sidemenu.php
@@ -6,13 +6,16 @@
         <a href="clients" class="">Clients</a>
     </li>
     <li>
-        <a href="issues" class="">Issues</a>
-    </li>
-    <li>
         <a href="projects" class="">Projects</a>
     </li>
     <li>
         <a href="tasks" class="">Tasks</a>
+    </li>
+    <li>
+        <a href="teams" class="">Teams</a>
+    </li>
+    <li>
+        <a href="tickets" class="">Tickets</a>
     </li>
     <li>
         <a href="users" class="">Users</a>
