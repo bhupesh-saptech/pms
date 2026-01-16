95236ec38e4cf03dd07ce889719bafd902708b13
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-17 05:01:48 +0530

    team Form created

--- app/Controllers/TeamsController.php
+++ app/Controllers/TeamsController.php
@@ -11,6 +11,14 @@ class TeamsController extends BaseController {
     protected $data;
     public function __construct() {
         $this->TeamsModel = new TeamsModel();
+        $this->data['status'] = [0=>'InActive',
+                                 1=>'Active'
+                                ];
+        $this->data['types'] = [ 0=>'Support',
+                                 1=>'Project',
+                                 2=>'Functional',
+                                 3=>'Technical'
+                                ];
         $this->data['teams'] = $this->TeamsModel->select('teams.*,agents.agent_nm as agent_nm')
                                                 ->join('agents','agents.agent_id = teams.agent_id','left')
                                                 ->findAll();
--- app/Views/teams/teamsForm.php
+++ app/Views/teams/teamsForm.php
@@ -6,7 +6,7 @@
     <form method="post" id="form">
         <div class="row">
             <div class="col-sm-9">
-                <h4 class="text-center">Issue Creation</h4>
+                <h4 class="text-center">team Creation</h4>
             </div>
             <div class="col-sm-3">                 
                 <button type="button" class="btn btn-secondary float-end " onclick="history.back();" ><i class="fa fa-arrow-left"></i></button>
@@ -14,42 +14,29 @@
                     <button type="submit" class="btn btn-success float-end me-3"><i class="fa fa-save"></i></button>
                 <?php else : ?>
                     <button type="button" class="btn btn-warning float-end me-3" onclick="btnToggle(this,event);"><i class="fa fa-edit"></i></button>
-                    <a href="issues/delete/<?= $issue->issue_id ?>" class="btn btn-danger float-end me-3" onclick="return confirm('Are you sure you want to delete this record?');"><i class="fa fa-trash"></i></a>
+                    <a href="teams/delete/<?= $team->team_id ?>" class="btn btn-danger float-end me-3" onclick="return confirm('Are you sure you want to delete this record?');"><i class="fa fa-trash"></i></a>
                 <?php endif ?>
-                <input  type="hidden" name="issue_id" id="issue_id" value="<?= set_value('issue_id', isset($issue->issue_id) ? $issue->issue_id : '') ?>">
+                <input  type="hidden" name="team_id" id="team_id" value="<?= set_value('team_id', isset($team->team_id) ? $team->team_id : '') ?>">
             </div>
         </div>
         <div class="form-group">
-            <label class="form-label">Issue Title</label>
-            <input class="form-control" type="text" name="issue_title" value="<?= set_value('issue_title', isset($issue->issue_title) ? $issue->issue_title : '') ?>" autocomplete="off">
-           
+            <label class="form-label">Team Code</label>
+            <input class="form-control" type="text" name="team_cd" value="<?= set_value('team_cd', isset($team->team_cd) ? $team->team_cd : '') ?>" autocomplete="off">
         </div> 
         <div class="form-group">
-            <label class="form-label">Project ID</label>
-            <select class="form-select select2" name="project_id" id="project_id">
-                <?php foreach($projects as $project) : ?>
-                <option value="<?= $project->project_id; ?>"><?= $project->project_nm; ?></option>
+            <label class="form-label">Team Name</label>
+            <input class="form-control" type="text" name="team_nm" value="<?= set_value('team_nm', isset($team->team_nm) ? $team->team_nm : '') ?>" autocomplete="off">
+        </div>       
+        <div class="form-group">
+            <label class="form-label">Team Type</label>
+            <select class="form-select select2" name="team_ty" id="iss_type">
+                <?php foreach($types as $param=>$value) : ?>
+                <option value="<?= $param; ?>"><?= $value; ?></option>
             <?php    endforeach; ?>
             </select> 
         </div>
         <div class="form-group">
-            <label class="form-label">SAP Module</label>
-            <select class="form-select select2" name="sap_module" id="sap_module">
-                <?php foreach($module as $key=>$value) : ?>
-                <option value="<?= $key; ?>"><?= $value; ?></option>
-            <?php    endforeach; ?>
-            </select> 
-        </div>
-        <div class="form-group">
-            <label class="form-label">Issue Type</label>
-            <select class="form-select select2" name="iss_type" id="iss_type">
-                <?php foreach($types as $key=>$value) : ?>
-                <option value="<?= $key; ?>"><?= $value; ?></option>
-            <?php    endforeach; ?>
-            </select> 
-        </div>
-        <div class="form-group">
-            <label class="form-label">Agent ID</label>
+            <label class="form-label">Team Lead</label>
             <select class="form-select select2" name="agent_id" id="agent_id">
                 <?php foreach($agents as $agent) : ?>
                 <option value="<?= $agent->agent_id; ?>"><?= $agent->agent_nm; ?></option>
@@ -59,8 +46,8 @@
         <div class="form-group">
             <label class="form-label">Status</label>
             <select class="form-select select2" name="status" id="status">
-                <?php foreach($status as $key=>$value) : ?>
-                <option value="<?= $key; ?>"><?= $value; ?></option>
+                <?php foreach($status as $param=>$value) : ?>
+                <option value="<?= $param; ?>"><?= $value; ?></option>
             <?php    endforeach; ?>
             </select> 
         </div>
