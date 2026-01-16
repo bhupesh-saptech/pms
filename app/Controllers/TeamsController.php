1b20da1ff6d9f04a987868552c5a17fa006fcea2
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-17 05:11:12 +0530

    methods added

--- app/Controllers/TeamsController.php
+++ app/Controllers/TeamsController.php
@@ -19,8 +19,8 @@ class TeamsController extends BaseController {
                                  2=>'Functional',
                                  3=>'Technical'
                                 ];
-        $this->data['teams'] = $this->TeamsModel->select('teams.*,agents.agent_nm as agent_nm')
-                                                ->join('agents','agents.agent_id = teams.agent_id','left')
+        $this->data['teams'] = $this->TeamsModel->select('teams.*,teams.agent_nm as agent_nm')
+                                                ->join('teams','teams.agent_id = teams.agent_id','left')
                                                 ->findAll();
          $this->data['dash']   = $this->TeamsModel->dashboard();
 
@@ -28,5 +28,53 @@ class TeamsController extends BaseController {
     
     public function index() {
         return view('teams/teamsList',$this->data);
+    }
+ public function create() {
+        $this->data['mode'] = 'create';
+        if ($this->request->is('post')) {
+            $form = [
+                'team_cd'  => $this->request->getPost('team_cd'),
+                'team_nm'  => $this->request->getPost('team_nm'),
+                'team_ty'  => $this->request->getPost('team_ty'),
+                'agent_id' => $this->request->getPost('agent_id'),
+                'status'   => $this->request->getPost('status'),
+            ];
+            if($this->TeamsModel->insert($form,false)) {
+                return redirect()->to('/teams')->with('message',"Data Inserted Succefully");
+            } else {
+                $data['errors'] = $this->TeamsModel->errors();     
+                return view('teams/teamsForm', $this->data);  
+            }
+        } else {
+            return view('teams/teamsForm',$this->data);
+        }
+    }
+    public function update($team_id) {
+        $this->data['mode']  = 'view';
+        $this->data['agent'] = $this->TeamsModel->find($team_id);
+        if ($this->request->is('post')) {
+            $form = [
+                'team_cd'  => $this->request->getPost('team_cd'),
+                'team_nm'  => $this->request->getPost('team_nm'),
+                'team_ty'  => $this->request->getPost('team_ty'),
+                'agent_id' => $this->request->getPost('agent_id'),
+                'status'   => $this->request->getPost('status'),
+            ];
+            if($this->TeamsModel->update($team_id,$form)) {
+                return redirect()->back()->with('message',"Data Inserted Succefully");
+            } else {
+                $data['errors'] = $this->TeamsModel->errors();     
+                return view('teams/teamsForm', $this->data);  
+            }
+        } else {
+            return view('teams/teamsForm',$this->data);
+        }
+    }
+    public function delete($team_id) {
+       if($this->TeamsModel->delete($team_id)) {
+           return redirect()->back()->with('message',"record Deleted Successfully");
+       } else {
+
+       }
     }
 }
--- app/Models/TeamsModel.php
+++ app/Models/TeamsModel.php
@@ -12,7 +12,11 @@ class TeamsModel extends Model
     protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [ 'team_cd', 
+                                    'team_nm', 
+                                    'team_ty', 
+                                    'agent_id',
+                                    'status'  ];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
