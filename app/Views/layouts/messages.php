dc55875c31278d4f7a3ebed40348af9e8839d68f
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-18 18:36:15 +0530

    flash data included

--- app/Views/layouts/messages.php
+++ app/Views/layouts/messages.php
@@ -1,14 +1,8 @@
-<?php
-    if(isset($_SESSION['status'])) {
-?>
+<?php if (session()->getFlashdata('status')): ?>
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Hey!</strong> <?php echo $_SESSION['status']; ?>
+  <strong>Hey!</strong>  <?= session()->getFlashdata('status') ?>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
-<?php 
-        unset($_SESSION['status']);
-    }
-
-?>
+<?php endif;?>
--- app/Views/tickets/ticketsList.php
+++ app/Views/tickets/ticketsList.php
@@ -11,6 +11,7 @@
                 <div class="col-sm-4">
                     <h5 class="card-title">List of Tickets</h1>
                 </div>
+                <?= $this->include('layouts/messages'); ?>
                 <div class="col-sm-8">
                     <a href="tickets/create" class="btn btn-primary float-end">Create ticket</a>
                 </div>
