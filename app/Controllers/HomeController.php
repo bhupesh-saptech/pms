09be0085d2bbe6ff2e79691ecfbd74b48588d0b0
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-19 20:30:09 +0530

    outer join

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,16 +8,18 @@ use CodeIgniter\HTTP\ResponseInterface;
 class HomeController extends BaseController {
     public function index() {
         $dbs  = \Config\Database::connect(); 
-        $sql = "select agent_id,
-                    sum(case when status = 0 then 1 else 0 end ) as status_0,
-                    sum(case when status = 1 then 1 else 0 end ) as status_1,
-                    sum(case when status = 2 then 1 else 0 end ) as status_2,
-                    sum(case when status = 3 then 1 else 0 end ) as status_3,
-                    sum(case when status = 4 then 1 else 0 end ) as status_4,
-                    sum(case when status = 5 then 1 else 0 end ) as status_5,
-                    sum(case when status = 6 then 1 else 0 end ) as status_6
-                from tickets 
-                group by agent_id";
+        $sql = "select agents.agent_id,
+                    sum(case when tickets.status = 0 then 1 else 0 end ) as status_0,
+                    sum(case when tickets.status = 1 then 1 else 0 end ) as status_1,
+                    sum(case when tickets.status = 2 then 1 else 0 end ) as status_2,
+                    sum(case when tickets.status = 3 then 1 else 0 end ) as status_3,
+                    sum(case when tickets.status = 4 then 1 else 0 end ) as status_4,
+                    sum(case when tickets.status = 5 then 1 else 0 end ) as status_5,
+                    sum(case when tickets.status = 6 then 1 else 0 end ) as status_6
+                from agents 
+                outer join tickets
+                on tickets.agent_id = agents.agent_id
+                group by agents.agent_id";
         $qry = $dbs->query($sql);
         $data =  $qry->getRowObject();
         echo "<pre>";
