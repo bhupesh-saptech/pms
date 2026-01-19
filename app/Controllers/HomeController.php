226d5b778ab776028f75eb4df883c8d4b2d61b01
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-19 22:08:04 +0530

    commmit data

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,24 +6,23 @@ use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class HomeController extends BaseController {
+    protected $data;
     public function index() {
         $dbs  = \Config\Database::connect(); 
         $sql = "select agents.agent_id,
-                    sum(case when tickets.status = 0 then 1 else 0 end ) as status_0,
-                    sum(case when tickets.status = 1 then 1 else 0 end ) as status_1,
-                    sum(case when tickets.status = 2 then 1 else 0 end ) as status_2,
-                    sum(case when tickets.status = 3 then 1 else 0 end ) as status_3,
-                    sum(case when tickets.status = 4 then 1 else 0 end ) as status_4,
-                    sum(case when tickets.status = 5 then 1 else 0 end ) as status_5,
-                    sum(case when tickets.status = 6 then 1 else 0 end ) as status_6
+                    sum(case when tickets.status = 0 then 1 else 0 end ) as status_00,
+                    sum(case when tickets.status = 1 then 1 else 0 end ) as status_01,
+                    sum(case when tickets.status = 2 then 1 else 0 end ) as status_02,
+                    sum(case when tickets.status = 3 then 1 else 0 end ) as status_03,
+                    sum(case when tickets.status = 4 then 1 else 0 end ) as status_04,
+                    sum(case when tickets.status = 5 then 1 else 0 end ) as status_05,
+                    sum(case when tickets.status = 6 then 1 else 0 end ) as status_06
                 from agents 
                 left outer join tickets
                 on tickets.agent_id = agents.agent_id
                 group by agents.agent_id";
         $qry = $dbs->query($sql);
-        $data =  $qry->getResultObject();
-        echo "<pre>";
-        print_r($data);
-
+        $this->data['list'] =  $qry->getResultObject();
+        return view('home/matrix',$this->data);
     }
 }
---
+++ app/Views/home/matrix.php
@@ -0,0 +1,51 @@
+<?=  $this->extend('layouts/base'); ?>
+<?=  $this->section("dashboard"); ?>
+    <?= $this->include('layouts/dashboard'); ?>
+<?=  $this->endSection(); ?>
+<?=  $this->section("content"); ?>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h5 class="card-title">agentwise summary</h1>
+                </div>
+                <div class="col-sm-8">
+                </div>
+            </div>  
+        </div>
+        <div class="card-body">
+            <table class="table table-stripped table-bordered">
+                <tr>
+                    
+                    <th>Agent ID</th>
+                    <th>Status-00</th>
+                    <th>Status-01</th>
+                    <th>Status-02</th>
+                    <th>Status-03</th>
+                    <th>Status-04</th>
+                    <th>Status-05</th>
+                    <th>Status-06</th>
+                </tr>
+                <?php foreach($list as $item): ?>
+                    <tr>
+                        <td><?= $item->agent_id ?></td>
+                        <td><?= $task->status_00 ?></td>
+                        <td><?= $task->status_01 ?></td>
+                        <td><?= $task->status_02 ?></td>
+                        <td><?= $task->status_03 ?></td>
+                        <td><?= $task->status_04 ?></td>
+                        <td><?= $task->status_05 ?></td>
+                        <td><?= $task->status_06 ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </div>
+</div>
+<?=  $this->endSection(); ?>
+<?=  $this->section("side_bar"); ?>
+<div class="row">
+    <a href="users" class="">Users</a>
+</div> 
+<?= $this->endSection(); ?>
