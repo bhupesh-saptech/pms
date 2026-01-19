167a4bae81a575c7635cc6c55af3c9a9540ed7a8
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2026-01-19 22:27:29 +0530

    status variable added

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,6 +12,13 @@ class HomeController extends BaseController {
     protected $data;
     public function __construct(){
         $this->ProjectModel = new ProjectsModel();
+        $this->data['status'] = [   0=>'New/Created',
+                                    1=>'Assigned',
+                                    2=>'In Progress',
+                                    3=>'On Hold/Blocked',
+                                    4=>'Resolved',
+                                    5=>'Closed',
+                                ];
     }
        
     public function index() {
